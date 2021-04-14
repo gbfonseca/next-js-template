ecebae63d96d62f880cdb31dc5185b73570480be
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-04-14 12:54:23 -0300

    chore: initial structure

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF'
+  },
+  spacings: {
+    small: '1rem',
+    medium: '2rem',
+    large: '3rem',
+  },
+  font: {
+    sizes: {
+      small: '1.2rem',
+      medium: '1.8rem',
+      large: '2rem',
+      superLarge: '5rem',
+    },
+  },
+};
